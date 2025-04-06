6ef9a7d42b28d86b80865e7366c468e53dadf771
Md Rahat Islam <islammdrahat308@gmail.com>
2025-04-06 23:47:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+ const logi = useState(1);
+ console.log(logi);
+
+ return (
+  <>
+   <p className="bg-red-400">Click on the Vite and React logos to learn more</p>
+  </>
+ );
+}
+
+export default App;
